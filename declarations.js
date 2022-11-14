5bb25d651de3fa361dea8dc9f882c5f843235fc3
Dulat_Nurmeden <nurmeden.02@gmail.com>
2022-11-14 12:59:05 +0600

    commit

--- declarations.js
+++ declarations.js
@@ -1,4 +1,11 @@
 const escapeStr = "\"`\\/\'"
 const arr = [4,'2']
 const obj = {str:"Dulat",num:20,bool:true,undef:undefined}
-const nested = {arr: [4,undefined,'2'],obj: {str:"Mirat", num:0,bool:true}}
+const nested = {
+    arr: [4,undefined,'2'], 
+    obj: {
+        str:"Mirat", 
+        num:0,
+        bool:true
+    }
+}
