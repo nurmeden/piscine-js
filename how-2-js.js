1372c063bd8eac5ef89177743bcc0bfb9bce9f2b
Dulat_Nurmeden <nurmeden.02@gmail.com>
2022-11-14 13:01:34 +0600

    commit

--- how-2-js.js
+++ how-2-js.js
@@ -1,1 +1,13 @@
 console.log('Hello World')
+const escapeStr = "\"`\\/\'"
+const arr = [4,'2']
+const obj = {str:"Dulat",num:20,bool:true,undef:undefined}
+const nested = {
+    arr: [4,undefined,'2'], 
+    obj: {
+        str:"", 
+        num:0,
+        bool:true
+    }
+}
+console.log(nested.obj)
