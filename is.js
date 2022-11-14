50c4c89d56811332948234e93fb287b3c291f560
Dulat_Nurmeden <nurmeden.02@gmail.com>
2022-11-14 20:18:19 +0600

    commit

--- is.js
+++ is.js
@@ -1,46 +1,46 @@
 const is = {}
 is.num = function(n) {
-    checker('number',n)
+    return checker('number',n)
 }
 is.nan = function(n) {
-    checker('NaN',n)
+    return checker('NaN',n)
 }
 is.str = function(n) {
-    checker('string',n)
+    return checker('string',n)
 }
 is.bool = function(n) {
-    checker('boolean',n)
+    return checker('boolean',n)
 }
 is.undef = function(n) {
-    checker('undefined',n)
+    return checker('undefined',n)
 }
 is.def = function(n) {
-    checker('defined',n)
+    return checker('defined',n)
 }
 is.arr = function(n) {
-    checker('array',n)
+    return checker('array',n)
 }
 is.obj = function(n) {
     if (checker('object',n) === false) {
-        checker('null'),n
+        return checker('null',n)
     } else {
-        checker('object',n)
+        return checker('object',n)
     }
 }
 is.fun = function(n) {
-    checker('function',n)
+    return checker('function',n)
 }
 is.truthy = function(n) {
-    checker('truthy',n)
+    return checker('truthy',n)
 }
 is.falsy = function(n) {
-    checker('falsy',n)
+    return checker('falsy',n)
 }
 
 function checker(type, n) {
     if (type === typeof(n)) {
-        console.log(true)
+        return true
     } else {
-        console.log(false)
+        return false
     }
 }
