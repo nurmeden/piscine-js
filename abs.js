f7b744e3a97cb4d1490409273cd7b95e19410ca9
Dulat_Nurmeden <nurmeden.02@gmail.com>
2022-11-14 18:57:17 +0600

    commit

--- abs.js
+++ abs.js
@@ -6,6 +6,16 @@ function isPositive(n) {
     }
 }
 
-function abs(digit) {
-    return digit + (Infinity-digit)
+function abs(numb){
+    if (numb==0){
+        return 0
+    }else if (isPositive(numb)){
+        return numb
+    }else{
+        return numb*-1
+    }
 }
+
+// function abs(digit) {
+//     return digit + (Infinity-digit)
+// }
