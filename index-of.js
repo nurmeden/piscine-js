15be172ab87d9cf41a7f8184587dd10dc1381a1f
Dulat_Nurmeden <nurmeden.02@gmail.com>
2022-11-17 21:09:50 +0600

    index-js

---
+++ index-of.js
@@ -0,0 +1,27 @@
+function indexOf(arr,word) {
+    for (let i = 0; i < arr.length; i++ ) {
+        if (arr[i] === word) {
+            return i
+        } 
+    }
+    return -1
+}
+
+function lastIndexOf(arr,word) {
+    var idx = -1
+    for (let i = 0; i < arr.length; i++ ) {
+        if (arr[i] === word) {
+            idx = i
+        }
+    }
+    return idx
+}
+
+function includes(arr,word) {
+    for (let i = 0; i < arr.length; i++ ) {
+        if (arr[i] === word) {
+            return true
+        }
+    }
+    return false
+}
