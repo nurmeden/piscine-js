46e5aff65102e19dd37a79c99565e422cb9c7685
Dulat_Nurmeden <nurmeden.02@gmail.com>
2022-11-18 22:55:50 +0600

    fbhdf

--- molecules-cells.js
+++ molecules-cells.js
@@ -6,15 +6,17 @@ let molecules = {
 }
 function RNA(s) {
     let result = ""
-    for (i = 0 ; i < s.length; i++) {
-        if (s[i] === "G") {
-            result += molecules.prop1[1]
-        } else if (s[i] === "C") {
-            result += molecules.prop2[1]
-        } else if (s[i] === "T") {
-            result += molecules.prop3[1]
-        } else if (s[i] === "A") {
-            result += molecules.prop4[1]
+    if (s.length > 0) {
+        for (i = 0 ; i < s.length; i++) {
+            if (s[i] === "G") {
+                result += molecules.prop1[1]
+            } else if (s[i] === "C") {
+                result += molecules.prop2[1]
+            } else if (s[i] === "T") {
+                result += molecules.prop3[1]
+            } else if (s[i] === "A") {
+                result += molecules.prop4[1]
+            }
         }
     }
     return result
@@ -22,16 +24,18 @@ function RNA(s) {
 
 function DNA(s) {
     let result = ""
-    for (i = 0 ; i < s.length; i++) {
-        if (s[i] === "G") {
-            result += molecules.prop2[0]
-        } else if (s[i] === "C") {
-            result += molecules.prop1[0]
-        } else if (s[i] === "U") {
-            result += molecules.prop4[0]
-        } else if (s[i] === "A") {
-            result += molecules.prop3[0]
+    if (s.length > 0) {
+        for (i = 0 ; i < s.length; i++) {
+            if (s[i] === "G") {
+                result += molecules.prop2[0]
+            } else if (s[i] === "C") {
+                result += molecules.prop1[0]
+            } else if (s[i] === "U") {
+                result += molecules.prop4[0]
+            } else if (s[i] === "A") {
+                result += molecules.prop3[0]
+            }
         }
     }
-    return result 
+    return result
 }
