529088c1c3e157e59c227f379e9cd1073fa80760
Dulat_Nurmeden <dulat_nurmeden@noreply.example.org>
2022-11-18 20:37:26 +0000

    dd

---
+++ find-expression.js
@@ -0,0 +1,16 @@
+function findExpression(numb){
+    str = ''
+    for (let i = 1; i < numb; i++) {
+        if (i%2==0){
+            for (let j = 0; j < i/2; j++) {
+                str+='*2';
+                
+            }
+        }else if (i%4==0){
+            for (let j = 0; j< i/4; i++){
+                str+='+4'
+            }
+        };
+        
+    }
+}
