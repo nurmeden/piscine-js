9ccd72595bde5247c4cba91a3b1cf730aac2ce4b
Dulat_Nurmeden <nurmeden.02@gmail.com>
2022-11-18 19:40:44 +0600

    rhger

--- keep-cut.js
+++ keep-cut.js
@@ -1,11 +1,11 @@
 function cutFirst(s) {
-    newstr = s.replace(s[0],'')
+    var newstr = s.replace(s[0],'')
     newstr = s.replace(s[0],'')
     return newstr
 }
 
 function cutLast(s) {
-    newstr = s.replace(s[s.length-1],'')
+    var newstr = s.replace(s[s.length-1],'')
     newstr = s.replace(s[s.length-1],'')
     return s
 }
