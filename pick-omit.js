30a17f441109d3edf2a65a2f9a63b25b39c5c653
nurmeden <nurmeden.02@gmail.com>
2022-11-30 18:56:24 +0600

    pick-omit

---
+++ pick-omit.js
@@ -0,0 +1,28 @@
+
+func
+tion pick(obj,strArr){
+    let obj1={}
+    Object.keys(obj).forEach(key=>{
+      if (typeof strArr==='string'){
+        if (key===strArr){
+        obj1[key]=obj[key]
+        }
+      }else{
+          for (let i=0;i<strArr.length;i++){
+          if (key===strArr[i]){
+            obj1[key]=obj[key]
+          }
+        }
+      }
+    })
+    return obj1
+}
+function omit(obj, keys) {
+    let res = {}
+    Object.keys(obj).forEach(key => {
+        if (!keys.includes(key)) {
+            res[key] = obj[key]
+        }
+    })
+    return res
+}
